35afabc8be98e638173f3d3d19ad9a43d8287b21
kobe655218 <kobe655218@gmail.com>
2021-07-19 10:40:58 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask , url_for , render_template
+import pandas as pd
+import yfinance as yf
+from db.Mysql import  Mysql
+from db.Mogodb import Mongodb
+import json
+
+
+app =Flask("__name__")
+
+
+@app.route('/')
+def hello_world():
+    # stock = yf.Ticker('2330.TW')
+    # stock_price = stock.history(period='max')
+    # print(stock_price)
+    mogodb = Mongodb()
+    # mysql = Mysql()
+    branch_data = mogodb.Table_Branch()
+    # big3 = mysql.Table_big3()
+    # print(mogodb.Table_Branch())
+
+    merge_df = pd.concat([branch_data], join="inner", axis=1)
+    merge_df.index = merge_df.index.astype(str).format()
+
+
+    return render_template("startbootstrap-sb-admin-2-gh-pages/index.html" ,data =json.dumps(merge_df.to_dict()))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
