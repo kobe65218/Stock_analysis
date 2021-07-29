74844bfbca4e438d77d6e86b487efbec595c3f43
kobe <kobe655218@gmail.com>
2021-07-29 23:08:44 +0800

    add docker

--- stock_dashboard/__init__.py
+++ stock_dashboard/__init__.py
@@ -4,6 +4,6 @@ from flask_sqlalchemy import  SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:kobe910018@mysql:3306/stock_analysis"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:kobe910018@kobe655218.ddns.net:3306/stock_analysis"
 db = SQLAlchemy(app)
 from stock_dashboard import route
