b8021045f68a8d195dc0c9b911e7557615c276f3
kobe <kobe655218@gmail.com>
2021-08-02 18:15:23 +0800

    change ansible playbook

--- dags/stock_dag.py
+++ dags/stock_dag.py
@@ -24,7 +24,7 @@ default_args = {
 stock_id_list =  ["2330" ,"2603","2609","3481","2303","2409","2317","2002"]
 
 
-with DAG('stock_dag', start_date= datetime(2021, 7, 29 , tzinfo=local_tz), schedule_interval="00 17 1-5 * *", tags=["stock"] ) as dag:
+with DAG('stock_dag', start_date= datetime(2021, 7, 29 , tzinfo=local_tz), schedule_interval="00 19 1-5 * *", tags=["stock"] ) as dag:
     crawl_web = PythonOperator(
         task_id='crawl_web',
         python_callable=run_crawl,
